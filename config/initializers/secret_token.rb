30a27a47a3fa6b636b022d74e90cdbd1d73622c9
Aonghus Flynn <aonghusflynn@gmail.com>
2013-06-18 16:59:39 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neighboursafe::Application.config.secret_token = '608a86bca0d13bbec9d11651eb6b63ef9d2e49fe580ee75504c154b7caa1f11bebc0b0fa8018be593647098f269fbfb42fb520bd3ab0423afe4d48dfdb88d8d4'
